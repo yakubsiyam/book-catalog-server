20563b13f4d17fb8f8da17c19ae0a93d072391d9
yakubsiyam <itsyakubsiyam@gmail.com>
2023-07-14 12:24:11 +0600

    Implementing winsto info logger and error logger

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+// import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+// import routes from "./app/routes";
+import httpStatus from 'http-status';
+// import cookieParser from "cookie-parser";
+const app: Application = express();
+
+app.use(cors());
+// app.use(cookieParser());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Application route
+// app.use('/api/v1/users', UserRoutes);
+// app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+// app.use("/api/v1", routes);
+
+// Testing
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   throw new Error('Testing error logger');
+// });
+
+// global error handler
+// app.use(globalErrorHandler);
+
+// handle not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API not found',
+      },
+    ],
+  });
+  next();
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { errorLogger, logger } from './shared/logger';
+import { Server } from 'http';
+
+let server: Server;
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    logger.info(`Database is connected successfully`);
+
+    server = app.listen(config.port, () => {
+      logger.info(`Application listening on port ${config.port}`);
+    });
+  } catch (err) {
+    errorLogger.error(`Failed to connect database`, err);
+  }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+bootstrap();
