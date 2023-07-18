a2e61ea279ec9bf25ddd972c0cccd4028e93986a
yakubsiyam <itsyakubsiyam@gmail.com>
2023-07-18 20:45:48 +0600

    Code update

--- src/helpers/paginationHelpers.ts
+++ src/helpers/paginationHelpers.ts
@@ -8,24 +8,23 @@ type IOptions = {
 };
 
 type IOptionsResult = {
-  page: number;
-  limit: number;
-  skip: number;
-  sortBy: string;
-  sortOrder: SortOrder;
+  page?: number;
+  limit?: number;
+  skip?: number;
+  sortBy?: string;
+  sortOrder?: SortOrder;
 };
 const calculatePagination = (options: IOptions): IOptionsResult => {
   const page = Number(options.page || 1);
-  const limit = Number(options.limit || 10);
-  const skip = (page - 1) * limit;
+  // const limit = Number(options.limit || 10);
+  // const skip = (page - 1) * limit;
 
   const sortBy = options.sortBy || 'createdAt';
   const sortOrder = options.sortOrder || 'desc';
 
   return {
     page,
-    limit,
-    skip,
+    // skip,
     sortBy,
     sortOrder,
   };
